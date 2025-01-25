b3d4bf4b1385cb0e97cbeb384e6078f506ec24de
nzhussup <nurzhanat2002@gmail.com>
2025-01-25 13:03:52 +0100

    ✨ Change the base api url

--- frontend/src/config/ConfigVariables.jsx
+++ frontend/src/config/ConfigVariables.jsx
@@ -1,5 +1,5 @@
 const config = {
-  apiUrl: "https://api.nzhussup.com/api/v1",
+  apiUrl: "https://api.nzhussup.com/api/v1/base",
   authUrl: "https://api.nzhussup.com/auth/v1",
   showLoadingDelay: 500,
   showNoInfoDelay: 500,
