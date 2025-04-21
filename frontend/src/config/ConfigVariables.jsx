59363506d688e698829d98b4124b6a034c90c5f6
nzhussup <nurzhanat2002@gmail.com>
2025-04-21 16:26:04 +0200

    ✨ Adjust the api path in the frontend

--- frontend/src/config/ConfigVariables.jsx
+++ frontend/src/config/ConfigVariables.jsx
@@ -1,6 +1,6 @@
 const config = {
   apiBase: "https://api.nzhussup.com",
-  apiUrl: "https://api.nzhussup.com/api/v1",
+  apiUrl: "https://api.nzhussup.com/v1",
   authUrl: "https://api.nzhussup.com/auth",
   showLoadingDelay: 500,
   showNoInfoDelay: 500,
