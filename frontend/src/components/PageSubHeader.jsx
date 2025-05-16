efcbc40bfb4104f1f5e8ea8c964e62cdf50eaea1
nzhussup <nurzhanat2002@gmail.com>
2025-05-16 17:50:35 +0200

    ✨ Enahnce button alignment

--- frontend/src/components/PageSubHeader.jsx
+++ frontend/src/components/PageSubHeader.jsx
@@ -1,15 +1,19 @@
-import React, { Children } from "react";
+import React from "react";
 import BackArrow from "./BackArrow";
 import SortButton from "./SortButton";
 
 const PageSubHeader = ({ toggleSort, children }) => {
   return (
-    <div className='d-flex justify-content-between align-items-center'>
-      <div className='me-auto'>
+    <div className='d-flex justify-content-between align-items-center flex-nowrap w-100'>
+      <div className='me-2'>
         <BackArrow />
       </div>
-      {children}
-      <div className='ms-auto'>
+
+      <div className='flex-grow-1 d-flex justify-content-center'>
+        {children}
+      </div>
+
+      <div className='ms-2'>
         <SortButton onSort={toggleSort} />
       </div>
     </div>
